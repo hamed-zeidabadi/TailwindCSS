971e2e1cc4a62f172862a90e7420d81fca4e40b9
Hamed Zeidabadi <zeidabadi73@gmail.com>
2022-07-28 14:18:41 +0430

    add file
    add file with upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dist/**/*.{html,js}',
+    './index.html',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
